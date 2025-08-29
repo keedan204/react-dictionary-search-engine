c4203e1ee3019c015cedd5cc0fc16419d7dd4c5a
KeeDan204 <danikaymonique@gmail.com>
2025-08-29 15:07:23 -0500

    Big update, added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Dictionary from "./Dictionary";
+import Footer from "./footer";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="text-center">📚 Dictionary App</h1>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <Footer className="text-center" />
     </div>
   );
 }
-
-export default App;
