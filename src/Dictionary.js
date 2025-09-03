e4f5883fc167d90d9b59676f0b6442a074e71db7
KeeDan204 <danikaymonique@gmail.com>
2025-09-03 15:52:48 -0500

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,54 +1,54 @@
+
 import React, { useState } from "react";
 import axios from "axios";
-import Meaning from "./Definition";
-import "./App.css";
+import Definitions from "./Definitions";
+import "./Dictionary.css";
+import Synonyms from "./Synonyms";
+import Phonetics from "./Phonetics";
+import Photos from "./Photos";
 
 export default function Dictionary() {
-  let [word, setWord] = useState("");
-  let [results, setResults] = useState(null);
-
-  function handleWordChange(event) {
-    setWord(event.target.value);
-  }
+  let [keyword, setKeyword] = useState("");
+  let [definitions, setDefinitions] = useState(null);
+  let [phonetic, setPhonetic] = useState("");
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
-    setResults(response.data);
+    console.log(response.data);
+    setDefinitions(response.data.meanings);
+    setPhonetic(response.data.phonetic);
+  }
+
+  function handleImageResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
     event.preventDefault();
-    if (word) {
-      let key = "f063aad8tb9d2a804775off7e6bf14bb";
-      let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${key}`;
-      axios
-        .get(apiUrl)
-        .then(handleResponse)
-        .catch(function (error) {
-          console.error("Error fetching the definition:", error);
-        });
-    }
+    let apiKey = "f063aad8tb9d2a804775off7e6bf14bb";
+    let apiRrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiRrl).then(handleResponse);
+
+    let imgApiKey = "f063aad8tb9d2a804775off7e6bf14bb";
+    let imgApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imgApiKey}`;
+    axios.get(imgApiUrl).then(handleImageResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
   }
 
   return (
-    <div className="Dictionary">
+    <div className="Dictionary text-center">
       <form onSubmit={search}>
-        <input
-          type="search"
-          placeholder="Search for a word..."
-          onChange={handleWordChange}
-        />
+        <input type="search" onChange={handleKeywordChange} autoFocus={true} />
       </form>
 
-      {word && (
-        <p className="searching">
-          Searching for: <strong>{word}</strong>
-        </p>
-      )}
-
-      {results &&
-        results.meanings.map(function (meaning, index) {
-          return <Meaning meaning={meaning} key={index} />;
-        })}
+      <Definitions definitions={definitions} word={keyword} />
+      <Phonetics phonetic={phonetic} />
+      <Synonyms definitions={definitions} />
+      <Photos photos={photos} />
     </div>
   );
 }
