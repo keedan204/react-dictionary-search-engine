4a700c02fec98860f2bed425b712fbfc33b29231
KeeDan204 <danikaymonique@gmail.com>
2025-09-03 15:51:19 -0500

    Update Photos.js

---
+++ src/Photos.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  if (props.photos) {
+    console.log(props.photos);
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.slice(0, 4).map(function (photo, index) {
+            return (
+              <div className="col-6" key={index}>
+                <a
+                  href={photo.src.original}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={photo.src.landscape} alt="" className="img-fluid" />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
