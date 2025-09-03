3b44e3f7439413bf9e6f97210d72a38a081dac07
KeeDan204 <danikaymonique@gmail.com>
2025-09-03 15:54:06 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Dictionary from "./Dictionary";
-import "./App.css";
+import Footer from "./footer";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>📚 Dictionary App</h1>
-    <small>Coded by KeeDan204</small>
-      <Dictionary />
+      <div className="container">
+        <h1 className="text-center">📚 Dictionary App</h1>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <Footer className="text-center" />
     </div>
   );
 }
