ae9c1f66d1d2f31f84558444f329eaeeb77d6e0a
KeeDan204 <danikaymonique@gmail.com>
2025-09-03 15:53:21 -0500

    Update Definitions.js

--- src/Definitions.js
+++ src/Definitions.js
@@ -1,54 +1,23 @@
-import React, { useState } from "react";
-import axios from "axios";
-import Meaning from "./Definition";
-import "./App.css";
 
-export default function Dictionary() {
-  let [word, setWord] = useState("");
-  let [results, setResults] = useState(null);
+import React from "react";
 
-  function handleWordChange(event) {
-    setWord(event.target.value);
-  }
+export default function Definitions(props) {
+  if (props.definitions && props.definitions.length > 0) {
+    const firstMeaning = props.definitions[1];
 
-  function handleResponse(response) {
-    setResults(response.data);
-  }
-
-  function search(event) {
-    event.preventDefault();
-    if (word) {
-      let key = "f063aad8tb9d2a804775off7e6bf14bb";
-      let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${key}`;
-      axios
-        .get(apiUrl)
-        .then(handleResponse)
-        .catch(function (error) {
-          console.error("Error fetching the definition:", error);
-        });
-    }
-  }
-
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          placeholder="Search for a word..."
-          onChange={handleWordChange}
-        />
-      </form>
-
-      {word && (
-        <p className="searching">
-          Searching for: <strong>{word}</strong>
+    return (
+      <div className="Definitions">
+        Definition for <strong>{props.word}</strong>
+        <p>
+          <strong>{firstMeaning.partOfSpeech}</strong>:{" "}
+          {firstMeaning.definition}
         </p>
-      )}
-
-      {results &&
-        results.meanings.map(function (meaning, index) {
-          return <Meaning meaning={meaning} key={index} />;
-        })}
-    </div>
-  );
+        {firstMeaning.example && (
+          <p className="example">Example: "{firstMeaning.example}"</p>
+        )}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
