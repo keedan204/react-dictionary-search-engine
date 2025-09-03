5aeb2d85ea1ed990aa35b3c7ba47aa3bae30b8d6
KeeDan204 <danikaymonique@gmail.com>
2025-09-03 15:43:20 -0500

    Update Phonetics.js

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,13 +1,1 @@
-import React from "react";
 
-export default function Phonetics(props) {
-  if (props.phonetic) {
-    return (
-      <div className="Phonetics">
-        <p>{props.phonetic}</p>
-      </div>
-    );
-  } else {
-    return null;
-  }
-}
